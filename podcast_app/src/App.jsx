40bf4663190909c2d5e375f4e6557ef7eb0a411d
keotshepileMaje <keotshepilemaje@gmail.com>
2023-07-25 10:58:12 +0200

    Combined components

--- podcast_app/src/App.jsx
+++ podcast_app/src/App.jsx
@@ -1,14 +1,74 @@
-// import { useState, useEffect} from 'react'
+import React from 'react'
 import './App.css'
+import ShowSeasons from './components/main/ShowSeasons'
+import AllShows from './components/main/AllShows'
 import Navbar from './components/header/Navbar'
+import data from './data'
 
-function App() {
+export default function App() {
+    const [preview, setPreview] = React.useState([])
 
-  return (
-    <>
-      <Navbar />
-    </>
-  )
+    const [showData, setShowData] = React.useState(data)
+
+    const [show, setShow] = React.useState(false)
+
+    // console.log(showData)
+
+    React.useEffect(
+        () => {
+            fetch('https://podcast-api.netlify.app/shows')
+                .then( res => res.json())
+                .then( data => setPreview(data))
+        }, []
+    )
+
+    function handleClick () {
+        console.log('Clicked')
+        setShow(
+            (prev) => !prev
+        )
+
+    }
+    const [showDescription, setShowDescription] = React.useState(false)
+
+    function toggleShowDescription() {
+        setShowDescription(
+            (prev) => !prev
+        )
+    }
+
+    const podcastTitle = preview.map(
+        (podcast) => {
+            return (
+                <AllShows
+                    key = {podcast.id} 
+                    image = {podcast.image}
+                    handleWatchShow = {podcast}
+                    title = {podcast.title}
+                    genres = {podcast.genres}
+                    handleClick = {handleClick}
+
+                    description = {podcast.description}
+
+                 />
+            )
+        }
+    )
+
+    return (
+        <div>
+            <Navbar />
+            <div>
+                <button
+                    onClick= {handleClick}
+                >Go back home</button>
+            </div>
+            <div>
+                {show ? 
+                <ShowSeasons data = {data} />:
+                <div className="show-container">{podcastTitle}</div>
+                }
+            </div>
+        </div>
+    )
 }
-
-export default App
