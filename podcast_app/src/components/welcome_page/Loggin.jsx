2a32f13a076716681b48f3fdbe6607af8a989251
keotshepileMaje <keotshepilemaje@gmail.com>
2023-07-26 12:16:12 +0200

    Addded Login page

---
+++ podcast_app/src/components/welcome_page/Loggin.jsx
@@ -0,0 +1,27 @@
+import Form from './form'
+import Navbar from '../header/Navbar'
+
+export default function Loggin () {
+
+    return (
+        <div>
+            <Navbar />
+            <Form />
+        </div>
+    )
+}
+
+/**
+ * The code from App
+ */
+
+// import React from 'react'
+// import Loggin from './components/welcome_page/Loggin'
+
+// export default function App() {
+//     return (
+//         <div>
+//             <Loggin />
+//         </div>
+//     )
+// }
