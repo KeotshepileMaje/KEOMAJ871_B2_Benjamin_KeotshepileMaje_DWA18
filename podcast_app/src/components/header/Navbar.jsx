ec914fe7f8f02bb3986d5fd1e30b7d941d28392d
keotshepileMaje <keotshepilemaje@gmail.com>
2023-07-31 15:58:52 +0200

    Removed search

--- podcast_app/src/components/header/Navbar.jsx
+++ podcast_app/src/components/header/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar () {
 
     return (
-        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-md sticky-top navbar-dark bg-dark">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">Fixed navbar</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,10 +19,6 @@ export default function Navbar () {
                         <a className="nav-link disabled" aria-disabled="true">Disabled</a>
                         </li>
                     </ul>
-                    <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
                 </div>
             </div>
         </nav> 
