b8bffb7b490bedd6571f6a0925e5fb070eb1d858
keotshepileMaje <keotshepilemaje@gmail.com>
2023-08-01 22:30:41 +0200

    removed to mainContent component

--- podcast_app/src/App.jsx
+++ podcast_app/src/App.jsx
@@ -2,27 +2,38 @@ import './App.css'
 import Navbar from './components/header/Navbar'
 // import data from './data'
 import MainContent from './components/MainContent'
-// import { useState } from 'react';
-import ThemeToggle from './components/header/Theme'
 
 export default function App() {
 
     return (
         <div>
-            <ThemeToggle />
             <Navbar />
-            <div className='content'>
-                <div className='sidebar'>
-                    <p>Favourite</p>
-                    <div>                    
-                    <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                    </div>
-                </div>
-                <MainContent />  
-            </div>      
+            <MainContent />  
         </div>
     )
 }
+
+// const defaultArrange = preview
+// const arrangeA_Z = [...preview].sort((a, b) => a.title.localeCompare(b.title))
+// const arrangeZ_A = [...preview].sort((a, b) => b.title.localeCompare(a.title))
+// const ascendingOrder = [...preview].sort((a, b) => new Date(a.updated) - new Date(b.updated))
+// const descendingOrder = [...preview].sort((a, b) => new Date(b.updated) - new Date(a.updated))
+
+// function abc() {
+// if (activeButton === 'Default' ) {
+//     return defaultArrange
+// }
+// if (activeButton === 'A-Z' ) {
+//     return arrangeA_Z
+// }
+// if (activeButton === 'Z-A' ) {
+//     return arrangeZ_A
+// }
+// if (activeButton === 'Latest date' ) {
+//     return ascendingOrder
+// }
+// if (activeButton === 'Oldest date' ) {
+//     return descendingOrder
+// }
+// }
+// console.log(abc)
