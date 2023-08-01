29b0193b92a31c15653e939da97127589332a4be
keotshepileMaje <keotshepilemaje@gmail.com>
2023-08-01 22:31:07 +0200

    Cleaning the unused code

--- podcast_app/src/App.jsx
+++ podcast_app/src/App.jsx
@@ -12,28 +12,3 @@ export default function App() {
         </div>
     )
 }
-
-// const defaultArrange = preview
-// const arrangeA_Z = [...preview].sort((a, b) => a.title.localeCompare(b.title))
-// const arrangeZ_A = [...preview].sort((a, b) => b.title.localeCompare(a.title))
-// const ascendingOrder = [...preview].sort((a, b) => new Date(a.updated) - new Date(b.updated))
-// const descendingOrder = [...preview].sort((a, b) => new Date(b.updated) - new Date(a.updated))
-
-// function abc() {
-// if (activeButton === 'Default' ) {
-//     return defaultArrange
-// }
-// if (activeButton === 'A-Z' ) {
-//     return arrangeA_Z
-// }
-// if (activeButton === 'Z-A' ) {
-//     return arrangeZ_A
-// }
-// if (activeButton === 'Latest date' ) {
-//     return ascendingOrder
-// }
-// if (activeButton === 'Oldest date' ) {
-//     return descendingOrder
-// }
-// }
-// console.log(abc)
