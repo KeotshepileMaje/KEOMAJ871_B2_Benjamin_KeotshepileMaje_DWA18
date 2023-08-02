e31be8a071cc8a5d1808a5c2b38de03443661dbd
keotshepileMaje <keotshepilemaje@gmail.com>
2023-08-02 17:54:46 +0200

    Removed fullscreen mode

--- podcast_app/src/components/main/EpisodesModal.jsx
+++ podcast_app/src/components/main/EpisodesModal.jsx
@@ -26,7 +26,6 @@ export default function EpisodesModal({ show, handleClose, selectedSeasonData })
             onHide={handleClose} 
             backdrop="static" 
             keyboard={false}
-            fullscreen
         >
         <Modal.Header >
             <div className='season-view'>
@@ -43,3 +42,27 @@ export default function EpisodesModal({ show, handleClose, selectedSeasonData })
     </Modal>
     );
 }
+
+
+// import './EpisodesModal.css'
+/*
+        <div className="overlay" data-list-active>
+    <div className="overlay__preview">
+        <img className="overlay__blur" data-list-blur src={selectedSeasonData.image} width= '300px'/>
+        <img className="overlay__image" data-list-image src={selectedSeasonData.image}/>
+    </div>
+
+    <div className="overlay__content">
+        <h3 className="overlay__title" data-list-title>
+            Season {selectedSeasonData.season}
+        </h3>
+        <div className="overlay__data" data-list-subtitle>
+            {mappingOverEpisodes(selectedSeasonData)}
+        </div>
+        <p className="overlay__data overlay__data_secondary" data-list-description></p>
+    </div>
+
+    <div className="overlay__row">
+        <button className="overlay__button overlay__button_primary" data-list-close onClick={handleClose}>Close</button>
+    </div>
+*/
