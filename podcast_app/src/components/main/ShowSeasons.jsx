96890de37cbefc65333ba31e753848c93dbd4f65
keotshepileMaje <keotshepilemaje@gmail.com>
2023-08-04 17:03:29 +0200

    Added styling component

--- podcast_app/src/components/main/ShowSeasons.jsx
+++ podcast_app/src/components/main/ShowSeasons.jsx
@@ -2,6 +2,7 @@ import './ShowSeasons.css'
 // import 'bootstrap/dist/css/bootstrap.css'
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import Modal from 'react-bootstrap/Modal';
 import EpisodesModal from './EpisodesModal'
 
@@ -17,7 +18,6 @@ export default function ShowSeason(props) {
             return prevSeason === seasonNumber ? null : seasonNumber 
             }
         );
-
     }
 
     function renderSeasons() {
@@ -25,12 +25,16 @@ export default function ShowSeason(props) {
             <div>
                 {props.data.seasons.map((seasonData) => (
                     <div key={seasonData.season}>
+                    <Card body>
                         <div
                             className='season_block' 
                             onClick={(() => handleSeasonClick(seasonData.season))}
+                            style={{display: 'flex'}}
                         >
-                            Season: {seasonData.season}
+                            <div style={{width: '50%'}}>Season: {seasonData.season}</div>
+                            <div>E:{seasonData.episodes.length}</div>
                         </div>
+                    </Card>
                     </div>
                     ))
                 }
@@ -46,6 +50,7 @@ export default function ShowSeason(props) {
     // Function to close the episodes modal
     function closeEpisodesModal() {
     setShowEpisodesModal(false);
+    props.handleClose();
     }
 
     return (
@@ -55,11 +60,25 @@ export default function ShowSeason(props) {
             onHide={props.handleClose}
             backdrop="static"
             keyboard={false}
+            size = 'xl'
             >
-                <Modal.Header closeButton>
-                    <Modal.Title>Number of Seasons: {props.data.seasons.length}</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
+            <Modal.Header closeButton>
+                <div className="card mb-3" style={{maxWidth: '540px'}}>
+                    <div className="row g-0">
+                        <div className="col-md-4">
+                            <img src={props.data.image} className="img-fluid rounded-start" />
+                        </div>
+                        <div className="col-md-8">
+                            <div className="card-body">
+                            <h5 className="card-title">{props.data.title}</h5>
+                            <p className="card-text">{props.data.description}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Modal.Header>
+
+                <Modal.Body> 
                     {renderSeasons()}
                 </Modal.Body>
                 <Modal.Footer>
@@ -68,11 +87,16 @@ export default function ShowSeason(props) {
                     </Button>
                 </Modal.Footer>
             </Modal>
-            {/* Render the episodes modal */}
+
+            <div>
+        </div>
             <EpisodesModal
-            show={showEpisodesModal}
-            handleClose={closeEpisodesModal}
-            selectedSeasonData={props.data.seasons.find((seasonData) => seasonData.season === selectSeason)}
+                show={showEpisodesModal}
+                handleClose={closeEpisodesModal}
+                selectedSeasonData={props.data.seasons.find((seasonData) => seasonData.season === selectSeason)}
+                seasons={props.data.seasons} 
+                selectedSeason={selectSeason}
+                showTitle={props.data.title}
             />
         </div>
     );
