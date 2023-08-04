52d746f122fc103225259d637c841c4e8cd62b84
keotshepileMaje <keotshepilemaje@gmail.com>
2023-08-04 17:02:43 +0200

    Add code to fetch

--- podcast_app/src/components/main/ShowCards.jsx
+++ podcast_app/src/components/main/ShowCards.jsx
@@ -1,15 +1,12 @@
-import React from 'react'
 import './ShowCards.css'
 import genres from '../../data/genreData'
 import Card from 'react-bootstrap/Card';
 import Spinner from 'react-bootstrap/Spinner';
-import Favourite from '../sidebar/Favourite';
 import { useState } from 'react';
 
 export default function ShowCards(props) {
 
-    const [showDescription, setShowDescription] = React.useState(false)
-    const [hovered, setHovered] = useState(false)
+    const [showDescription, setShowDescription] = useState(false)
 
     function toggleShowDescription() {
         setShowDescription(
@@ -22,24 +19,26 @@ export default function ShowCards(props) {
             const foundGenre = genres.find((genre) => genre.id === genreNum);
             return (
                 <span key={genreNum}>
-                    {foundGenre ? `${foundGenre.genre},` : "Unknown Genre"}
+                    {foundGenre ? foundGenre.genre : "Unknown Genre"}
                 </span>
             );
         })
+
         return podcast
     }
 
-    const [imageError, setImageError] = React.useState(false);
+    const [imageError, setImageError] = useState(false);
 
     const handleImageError = () => {
       setImageError(true);
     };
 
-    const [favourite, addFavorite] = useState(false)
-
-    function togggleFavourite() {
-        addFavorite((prev) => !prev)
-    }
+    //Time stamp to change into Lat updated into yy/mm/dd
+    function formatTimestamp(timestamp) {
+        const date = new Date(timestamp);
+        const formattedDate = `${date.getUTCDate().toString().padStart(2, '0')}${(date.getUTCMonth() + 1).toString().padStart(2, '0')}//${date.getUTCFullYear().toString().slice(2)}`;
+        return formattedDate;
+      }
     return (
         <Card className='custom-card'>
             {imageError ? 
@@ -66,17 +65,10 @@ export default function ShowCards(props) {
                 <div className='podcast_show_info'>
                     <div className='info-container'>
                         <span>S{props.numOfSeasons}</span>
-                        {
-                        showDescription && <span>Description: {props.description}</span>
-                        }
-                        <Favourite 
-                            favourite = {favourite}
-                            togggleFavourite = {togggleFavourite}
-                        />
                         <button onClick = {toggleShowDescription}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-down-circle-fill" viewBox="0 0 16 16">
-                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
-                        </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-down-circle-fill" viewBox="0 0 16 16">
+                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
+                            </svg>
                         </button>
                     </div>
                     <div className='genre' 
@@ -85,123 +77,14 @@ export default function ShowCards(props) {
                     >
                         {findGenre(props.genres)}
                     </div>
+                    <div className="card-text">
+                        <small className="text-body-secondary">Last updated {formatTimestamp(props.updated)}</small>
+                    </div>
+                    {
+                        showDescription && <span>Description: {props.description}</span>
+                        }
                 </div>
             </Card.Body>
         </Card>
     )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{/* <div className='show_preview'>
-<div className='podcast_show_image'
-    onClick = {props.handleClick}
-    >
-    <img src={props.image}  id={props.id}/>
-</div>
-<div className='podcast_show_info'>
-    <div>
-        <span>NumOfSeasons {props.updated}</span>
-        {
-            showDescription && <span>Description: {props.description}</span>
-        }
-        <span>★★★</span>
-        <span>❤</span>
-        <button onClick = {toggleShowDescription}>More</button>
-    </div>
-    <div>Genre: {findGenre(props.genres)}</div>
-</div>
-</div> */}
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react'
-// import './App.css'
-
-
-// export default function App() {
-
-//     return(
-//         <div className="card">
-//             <img src='/public/project-in-production.webp' /> 
-//             <div className="infoDispay">
-//                 <div className='infoOnshow'>
-//                     <span>NumOfSeasons</span>
-//                     <span>★★★</span>
-//                     <span>❤</span>
-//                     <span>More</span>
-//                 </div>
-//                 <h3>Heading</h3>
-//             </div>
-//         </div>
-//     )
-// }
-
-
-// const [selectSeason, setSelectSeason] = React.useState(null);
-
-// function handleSeasonClick(seasonNumber) {
-//     setSelectSeason(
-//         (prevSeason) => (prevSeason === seasonNumber ? null : seasonNumber)
-//     );
-// }
-
-// function renderSeasonsOrEpisodes() {
-//     return (
-//         <div>
-//             {props.data.seasons.map((seasonData) => (
-//                 <div key={seasonData.season}>
-//                     <h2 onClick={() => handleSeasonClick(seasonData.season)}>
-//                     Season: {seasonData.season}
-//                     </h2>
-//                     {
-//                         selectSeason === seasonData.season && 
-//                         renderEpisodes(seasonData)
-//                     }
-//                 </div>
-//                 ))
-//             }
-//         </div>
-//     );
-// }
-
-
-
-
