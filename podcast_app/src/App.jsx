82c112564fdc8589f5791db6d0dea9d88ff3cdab
keotshepileMaje <keotshepilemaje@gmail.com>
2023-08-05 23:57:09 +0200

    Added signin

--- podcast_app/src/App.jsx
+++ podcast_app/src/App.jsx
@@ -1,53 +1,37 @@
+import { useState } from 'react';
 import './App.css'
 import NavbarComponent from './components/header/Navbar'
-// import data from './data'
 import MainContent from './components/MainContent'
-// import FavouriteEpisode from './components/main/FetchFavouriteEpi'
-
-export default function App() {
-
+import Loggin from './components/welcome_page/Loggin';
+function Home() {
     return (
-        <div>
-            <NavbarComponent />
+        <div className='Home'>
+            <Navbar />
             <MainContent />  
-        </div>
+         </div>
     )
 }
 
-// import { useState } from 'react';
-// import './App.css'
-// import NavbarComponent from './components/header/Navbar'
-// import MainContent from './components/MainContent'
-// import Loggin from './components/welcome_page/Loggin';
-// function Home() {
-//     return (
-//         <div className='Home'>
-//             <Navbar />
-//             <MainContent />  
-//          </div>
-//     )
-// }
+export default function App() {
 
-// export default function App() {
+    const [loggInHome, setLogInHome] = useState(null)
 
-//     const [loggInHome, setLogInHome] = useState(null)
+    function handleLoggin(){
+        setLogInHome(true)
+    }
 
-//     function handleLoggin(){
-//         setLogInHome(true)
-//     }
+    return (
+        <div>
+            {
+                !loggInHome && 
+                <Loggin
+                    handleLoggin = {handleLoggin}
 
-//     return (
-//         <div>
-//             {
-//                 !loggInHome && 
-//                 <Loggin
-//                     handleLoggin = {handleLoggin}
-
-//                  /> 
-//             }
-//             {
-//                 loggInHome && <Home />
-//             }
-//         </div>
-//     )
-// }
+                 /> 
+            }
+            {
+                loggInHome && <Home />
+            }
+        </div>
+    )
+}
